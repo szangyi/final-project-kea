441f5a611c9c7551a062fd2480ceaee53fe95dbb
Alanis <alanismirko@gmail.com>
2023-05-16 21:52:56 +0200

    Upading table with profile results

--- frontend/src/components/Influencer/Profiles.js
+++ frontend/src/components/Influencer/Profiles.js
@@ -17,48 +17,77 @@ const Profiles = (props) => {
 
     const influencerData = props.influencerData;
     return (
-            <TableContainer component={Paper} elevation={0}>
-                <Table sx={{ minWidth: 650 }} >
-                    <TableHead>
-                        <TableRow>
-                            <TableCell></TableCell>
-                            <TableCell align="right">Name</TableCell>
-                            <TableCell align="right">Category</TableCell>
-                            <TableCell align="right">Linked accounts</TableCell>
-                            <TableCell></TableCell>
-                            <TableCell >
-                                <Button href="/create-profile" variant="outlined" startIcon={<AddIcon />}>
-                                    Add new profile
+        <TableContainer component={Paper} elevation={0}>
+            <Table sx={{ minWidth: 650 }} >
+                <TableHead>
+                    <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell align="right">Name</TableCell>
+                        <TableCell align="right">Category</TableCell>
+                        <TableCell align="right">Linked accounts</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell >
+                            <Button href="/create-profile" variant="outlined" startIcon={<AddIcon />}>
+                                Add new profile
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {influencerData.map((array, index) => (
+
+                        <TableRow
+                            key={index}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                Image
+                            </TableCell>
+                            <TableCell align="right">{array[2]}</TableCell>
+                            <TableCell align="right">{array[10]}</TableCell>
+                            <TableCell align="right" alignItems="center">
+                                {array[5] && (
+                                    <div>
+                                        <a href={`/${array[5]}`}>WEB</a>
+                                    </div>
+                                )}
+                                { array[6] && (
+                                    <div>
+                                        <a href={`/${array[6]}`}>IG</a>
+                                    </div>
+                                )}
+                                { array[7] && (
+                                    <div>
+                                        <a href={`/${array[6]}`}>YT</a>
+                                    </div>
+                                )}
+                                { array[8] && (
+                                    <div>
+                                        <a href={`/${array[6]}`}>TK</a>
+                                    </div>
+                                )}
+
+                            </TableCell>
+                            <TableCell align="right">
+                                <Button href="#" variant="outlined">
+                                    Edit
+                                </Button></TableCell>
+
+                            <TableCell align="left">
+                                <Button href="#" variant="outlined" >
+                                    Preview
                                 </Button>
                             </TableCell>
+                            <TableCell align="left">
+                                <Button href="#" variant="outlined">
+                                    Delete
+                                </Button></TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {influencerData.map((array, index) => (
-                            <TableRow
-                                key={index}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    Image
-                                </TableCell>
-                                <TableCell align="right">{array[2]}</TableCell>
-                                <TableCell align="right">{array[9]}</TableCell>
-                                <TableCell align="right">Linked accounts</TableCell>
-                                <TableCell align="right">
-                                    <Button href="#" variant="outlined">
-                                        Edit
-                                    </Button></TableCell>
-                                <TableCell align="left">
-                                    <Button href="#" variant="outlined" >
-                                        Preview
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 
 
