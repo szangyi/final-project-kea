843749d07f931dcd6a9d0520d39749dffdcceaaa
Alexandra Mészáros <alex069p@stud.kea.dk>
2023-05-29 18:01:31 +0200

    set authorization for frontend

--- backend/user_info_update.py
+++ backend/user_info_update.py
@@ -13,10 +13,14 @@ import json
 @post("/api/user_info_update")
 def _user_info_update():
     
-    token_request = request.headers.get('Authorization')
-    token_data = jwt.decode(token_request, g.SECRET_KEY, algorithms=["HS256"])
-    user_email = token_data["email"]
-    # user_email = "szangyi@gmail.com"
+    # --------------------------------------------------------------------------------------
+    # HAVE TO SET AUTHORIZATION ON THE FRONTEND OTHERWISE BACKEND CANNOT GET IT
+    # --------------------------------------------------------------------------------------
+
+    # token_request = request.headers.get('Authorization')
+    # token_data = jwt.decode(token_request, g.SECRET_KEY, algorithms=["HS256"])
+    # user_email = token_data["email"]
+    user_email = "alex@gmail.com"
 
 # VARIABLES ###########################
     request_user_data = request.json
