47ffbc308bc4ab1f41b810b420b4cce5c2791608
Alexandra Mészáros <alex069p@stud.kea.dk>
2023-06-12 16:04:47 +0200

    // if (!isCookie) {     //     return false     // } else {     //     return true;     // }
    not working, gives true even if I return none in the backend

--- backend/validate_cookie.py
+++ backend/validate_cookie.py
@@ -4,6 +4,7 @@ import helper_functions
 # VALIDATING THE COOKIE #
 @get("/api/validate-cookie")
 def _validate_cookie():
+
     try:
         print("######## VALIDATE COOOOOKIE")
 
@@ -15,14 +16,9 @@ def _validate_cookie():
 
         if onlyCheck is not None:
             print("######public pages")
-            selected_user_db = helper_functions._validation_function()
-            
-            if selected_user_db is not None:
-                response.status = 200
-            else:
-                print("# IGY JARTAL")
-                response.status = 200
-                return None
+            print("# THIS SHOULD BE NONE")
+            response.status = 200
+            return None
 
         else:
             print("######private pages")
@@ -32,9 +28,10 @@ def _validate_cookie():
                 response.status = 200
             else:
                 response.status = 400
-                return "Invalid request"  
+                print("# invalid request")
+                return None
+                # return "Invalid request"  
 
-        
     except Exception as ex:
         print(ex)
         response.status = 500
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,22 +1,17 @@
-import { useState } from "react";
 import { redirect } from "react-router-dom";
-import Cookies from 'js-cookie';
 import ValidateCookieAPI from "../api/ValidateCookieAPI";
 // return redirect('/login'); // redirect not working anymore
 
 
 export const authLoader = async (onlyCheck) => {
-    console.log('authLoader runnns')
-    console.log(onlyCheck)
     const isCookie = await ValidateCookieAPI(onlyCheck);
     console.log(isCookie)
-    
-    if (!isCookie) {
-        return false
-    } else {
-        return true;
-    }
 
+    // if (!isCookie) {
+    //     return false
+    // } else {
+    //     return true;
+    // }
 };
 
 export async function navLoader() {
@@ -34,16 +29,16 @@ export async function navLoader() {
 
 export async function navLoaderPublic() {
     console.log('navloaderPUBLICOOO runs')
-    const onlyCheck = "valami";
+    const onlyCheck = "whatever";
     const requestCookie = await authLoader(onlyCheck);
     console.log(requestCookie)
 
     if (requestCookie == true) {
         console.log('youare logged in')
-        return null
-        // return window.location.href = '/home';
+        return window.location.href = '/home';
     }
     else {
+        console.log('youare NOOTT logged in')
         return false;
     }
 }
